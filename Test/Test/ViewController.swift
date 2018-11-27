95f0de87dc8fff10303329c3c0226a3c76376929
王磊 <15840210488@163.com>
2018-11-27 14:21:08 +0800

    @IBInspectable的使用

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -44,5 +44,18 @@ struct Cat: Codable {
     let age: Int
 }
 
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
 
 

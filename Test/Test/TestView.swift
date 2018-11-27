1c3791ef774217b07988a9d6e0ef82ab11b972a7
Caesar Wang <wanglei@bingkongtech.com>
2018-11-27 14:35:52 +0800

    @IBDesignable的使用

---
+++ Test/Test/TestView.swift
@@ -0,0 +1,22 @@
+//
+//  TestView.swift
+//  Test
+//
+//  Created by 王磊 on 2018/11/27.
+//  Copyright © 2018 王磊. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
